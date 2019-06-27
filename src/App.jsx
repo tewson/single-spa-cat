4a7817e0be5c074ee7822fe11326a4b874acf9f2
Tewson Seeoun <tewson.seeoun@toasttab.com>
2019-06-27 18:18:18 +0100

    Set timeout to reset status

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,12 @@ const ACTION_STATUS = {
 
 export default () => {
   const [status, setStatus] = useState(STATUS.IDLE);
-  const handleSelectAction = action => setStatus(ACTION_STATUS[action]);
+  const handleSelectAction = action => {
+    setStatus(ACTION_STATUS[action]);
+    setTimeout(() => {
+      setStatus(STATUS.IDLE);
+    }, 1500);
+  };
   return (
     <>
       <h1>Cat!</h1>
